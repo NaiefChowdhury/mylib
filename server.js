874329f0c0b216c4273ca43c1e35099b92ad4eb0
Naief Chowdhury <rjsanto262@gmail.com>
2020-12-06 23:47:05 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,56 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+
+
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
+//Routes
+const indexRouter = require('./routes/index');
+
+
+
+
+const app = express();
+
+
+
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+
+
+
+
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+
+
+//mongoose
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to mongoose'));
+
+
+
+
+
+app.use('/', indexRouter);
+
+const PORT = process.env.PORT || 3000;
+
+
+
+app.listen(PORT, console.log("Server Is Going On: https://localhost:" + PORT));
